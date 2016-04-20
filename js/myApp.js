b70d897bd37cb655deeba69e30c17d9aba4a4091
jon-wade <jonwadeuk@gmail.com>
2016-04-20 13:15:36 +0100

    set-up basic layout, forms and variables

--- js/myApp.js
+++ js/myApp.js
@@ -1,4 +1,27 @@
-//main body goes here
-$(document).ready(function () {
 
-});
+angular.module('myApp', ['ngAnimate'])
+    .controller('myCtrl', function($scope){
+        //angular app code goes here
+
+
+        //variable that allows the display of a searching message
+        $scope.searching = true;
+
+        //variable that is true when data is found and allows the display of a confirmation message on the UI
+        $scope.dataReturned = true;
+
+        //variable that is true when an error occurs and allows the display of an error on the UI
+        $scope.errorReturned = true;
+
+        //run this when you hit submit
+        $scope.submit = function(){
+            alert('search pressed');
+        };
+
+
+
+
+
+
+
+    });
