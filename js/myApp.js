5ce86bdf0f82882a0e5af0f89342c7a3545a0ac8
jon-wade <jonwadeuk@gmail.com>
2016-04-20 14:07:05 +0100

    add in http data retreival logic and change success messages to be dynamic

--- js/myApp.js
+++ js/myApp.js
@@ -1,6 +1,6 @@
 
 angular.module('myApp', ['ngAnimate'])
-    .controller('myCtrl', function($scope){
+    .controller('myCtrl', function($scope, $http){
         //angular app code goes here
 
 
@@ -8,35 +8,72 @@ angular.module('myApp', ['ngAnimate'])
         $scope.searching = true;
 
         //variable that is true when data is found and allows the display of a confirmation message on the UI
-        $scope.dataReturned = true;
+        $scope.dataReturned = false;
 
         //variable that is true when an error occurs and allows the display of an error on the UI
-        $scope.errorReturned = true;
+        $scope.errorReturned = false;
 
         //variable to store the search term
         $scope.searchTerm='';
 
+        //variable to store number of photos returned to be displayed on the UI
+        $scope.totalPhotos;
+
         //run this when you hit submit
         $scope.submit = function(){
+
+            //store search term in a variable to allow us to clear the input box
             $scope.searchTerm = $scope.inputBox;
+
+            //clear the input box
             $scope.inputBox = '';
-            alert($scope.searchTerm);
+
+            //perform the JSON call to the API
+            getData();
 
 
 
         };
 
 
+        var getData = function(){
+            var url = 'https://api.flickr.com/services/rest';
+            var params = {
+                method: 'flickr.photos.search',
+                api_key: 'd8cdc8ec622bb63d150201cefee6ccb5',
+                tags: $scope.searchTerm,
+                format: 'json',
+                nojsoncallback: 1
+            };
+
+            $http({
+                url:url,
+                params: params,
+                method: 'GET'
+            })
+                .success(function(response){
+                    console.log('SUCCESS!');
+                    console.log(response);
+
+                    //set the totalPhotos variable ready for display on screen
+                    $scope.totalPhotos = response.photos.total;
+
+                    //display the dataReturned message
+                    $scope.dataReturned = true;
 
 
-        var url = 'https://api.flickr.com/services/rest';
-        var params = {
-            method: 'flickr.photos.search',
-            //api_key: api_key,
-            //tags: search_tag,
-            format: 'json',
-            nojsoncallback: 1
+                })
+                .error(function(response){
+                    console.log('ERROR!');
+                    console.log(response);
+
+                    //display an error message
+                    $scope.errorReturned = true;
+
+                })
         };
+
+
 
 
 
